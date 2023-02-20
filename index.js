c5da02883f1aaeccb4e3963530c48b339b792b37
Lahrasab <76862693+thespecialone1@users.noreply.github.com>
2023-02-20 14:44:39 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,77 +1,77 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
 const axios = require('axios');
-const { Client, LocalAuth} = require('whatsapp-web.js');
+const cheerio = require('cheerio');
 
 const client = new Client({
-    authStrategy: new LocalAuth()
+  authStrategy: new LocalAuth()
 });
+const newsApiUrl = 'http://20.203.148.130:8000/';
 
 client.on('qr', (qr) => {
-    qrcode.generate(qr, {small: true});
+    qrcode.generate(qr, { small: true });
 });
 
 client.on('ready', () => {
     console.log('Client is ready!');
 });
 
-client.initialize();
-
 const helpMessage = `Type 'news' to get the latest articles. Type 'more' to get more articles.`;
 
-client.on('message', async (message) => {
-    const lowerCase = message.body.toLowerCase();
-
-    if (lowerCase === 'news') {
+client.on('message', async (msg) => {
+  const lowerCase = msg.body.toLowerCase();
+    if (msg.body === 'news') {
         try {
-            const response = await axios.get('http://20.203.148.130:8000/');
-            const articles = response.data;
+            const response = await axios.get(newsApiUrl);
+            const articles = response.data.slice(0, 3);
+            let articleIndex = 0;
 
-            const articleCount = Math.min(3, articles.length); // limit to maximum of 3 articles
-            let index = 0;
+            for (const article of articles) {
+                const { title, source, description } = article;
+                const message = `*${title}*\n\n_${source}_\n\n${description}`;
 
-            for (let i = 0; i < articleCount; i++) {
-                const article = articles[index];
-                await message.reply(`*${article.title}*\n\n${article.description}\n\nSource: ${article.source}`);
-                index++;
+                await msg.reply(message);
+                articleIndex++;
             }
 
-            if (articleCount < articles.length) {
-                await message.reply(`Type 'more' to get more articles.`);
-            }
-
-        } 
-        catch (error) {
-            console.error(error);
-            await message.reply('Sorry, an error occurred while fetching the latest news.');
-        }
-    } else if (lowerCase === 'more') {
-        try {
-            const response = await axios.get('http://20.203.148.130:8000/');
-            const articles = response.data;
-
-            const articleCount = Math.min(3, articles.length - 3); // get next 3 articles
-            let index = 3;
-
-            if (articleCount === 0) {
-                await message.reply(`There are no more articles available.`);
+            if (articles.length === 0) {
+                await msg.reply('No news articles found');
+            } else if (articles.length < 3) {
+                await msg.reply('No more news articles available');
             } else {
-                for (let i = 0; i < articleCount; i++) {
-                    const article = articles[index];
-                    await message.reply(`*${article.title}*\n\n${article.description}\n\nSource: ${article.source}`);
-                    index++;
-                }
-
-                if (articleCount < articles.length - 3) {
-                    await message.reply(`Type 'more' to get more articles.`);
-                }
+                await msg.reply('Send "more" to get the next three news articles');
             }
-
         } catch (error) {
             console.error(error);
-            await message.reply('Sorry, an error occurred while fetching the latest news.');
+            await msg.reply('Failed to fetch news articles');
         }
-    } else if (lowerCase === 'help') {
-        await message.reply(helpMessage);
-    } else {
-        await message.reply('Hi! How may I help you? Type *help* to see the available commands.');
+    } else if (msg.body === 'help') {
+        const message = 'Available commands:\n\n- news: Get the latest news articles\n- help: Display this help message';
+        await msg.reply(message);
+    } else if (msg.body === 'more') {
+        try {
+            const response = await axios.get(newsApiUrl);
+            const articles = response.data.slice(3, 6);
+            let articleIndex = 0;
+
+            for (const article of articles) {
+                const { title, source, description } = article;
+                const message = `*${title}*\n\n_${source}_\n\n${description}`;
+
+                await msg.reply(message);
+                articleIndex++;
+            }
+
+            if (articles.length === 0) {
+                await msg.reply('No more news articles available');
+            } else {
+                await msg.reply('Send "more" to get the next three news articles');
+            }
+        } catch (error) {
+            console.error(error);
+            await msg.reply('Failed to fetch news articles');
+        }
     }
 });
+
+client.initialize();
